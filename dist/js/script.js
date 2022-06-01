7218d258ec8ce961b989f1282971147ac46d08c1
Vladislav Sokolov <79214579055@yandex.ru>
2022-06-01 16:02:15 +0300

    test commit

---
+++ dist/js/script.js
@@ -0,0 +1,27 @@
+window.addEventListener('DOMContentLoaded', () => {
+	// hamburger switch classes and navbar
+	const hamburger = document.querySelector('.header__hamburger');
+	const navbar = document.querySelector('.header-navbar');
+
+	hamburger.addEventListener('click', () =>{
+		if(hamburger.classList.contains('header__hamburger_active')) {
+			hamburger.classList.remove('header__hamburger_active');
+			navbar.classList.remove('header-navbar_active');
+		} else {
+			hamburger.classList.add('header__hamburger_active');
+			navbar.classList.add('header-navbar_active');
+		}
+	});
+	// Search
+	const searchIcon = document.querySelector('.header__loop');
+
+	searchIcon.addEventListener('click', () => {
+		if(searchIcon.classList.contains('header__loop_active')) {
+			searchIcon.classList.remove('header__loop_active');
+		} else {
+			searchIcon.classList.add('header__loop_active');
+		}
+	});
+
+	console.log('js gulp is working');
+});
---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+const {src, dest, series} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const csso = require('gulp-csso');
+const include = require('gulp-file-include');
+const htmlmin = require('gulp-htmlmin');
+const del = require('del');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const { watch } = require('browser-sync');
+const sync = require('browser-sync').create();
+
+function html(){
+    return src('src/**.html')
+        .pipe(include({
+            prefix: '@@'
+        }))
+        .pipe(htmlmin({
+            collapseWhitespace: true
+        }))
+        .pipe(dest('dist'))
+}
+
+function scss() {
+    return src('src/sass/**.scss')
+        .pipe(sass({
+            implementation: require('node-sass')
+          }))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions']
+        }))
+        .pipe(csso())
+        .pipe(concat('css/style.css'))
+        .pipe(dest('dist'))
+}
+
+function js(){
+    return src('src/js/**.js')
+        .pipe(dest('dist/js'))
+}
+
+function clear(){
+    return del('dist');
+}
+
+function serve(){
+    sync.init({
+        server: 'dist'
+    });
+
+    sync.watch('src/**.html', series(html)).on('change', sync.reload);
+    sync.watch('src/sass/**.scss', series(scss)).on('change', sync.reload);
+    sync.watch('src/js/**.js', series(js)).on('change', sync.reload);
+}
+
+function image(){
+    return src('src/img/**')
+        .pipe(imagemin())
+        .pipe(dest('dist/img/'))
+}
+
+exports.html = html;
+exports.scss = scss;
+exports.serve = series(clear, scss, html, js, image, serve);
+exports.build = series(clear, scss, html, js, image);
+exports.clear = clear;
